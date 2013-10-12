e71e01c88271a0fcecf591060f00774cd4fd0bce
ardekani <dehestan@usc.edu>
2013-10-11 18:55:24 -0700

    The main file is changes to go over the files that the smoothed file does not exist
    the smoothed file with size zero should be removed!

--- filterAndSmoothSticklebackPositionTrack_main.r
+++ filterAndSmoothSticklebackPositionTrack_main.r
@@ -25,25 +25,33 @@ source('C:/Users/dehestan/Dropbox/stickleback_r_newScript/Stickleback-Tracking-R
 inputListOfFiles = 'C:/Users/dehestan/Dropbox/stickleback_r_newScript/data/PJxPJ_F2_2011/videoinfo1of4_fullPath_schoolOnly_changedAddress.csv';
 listOfFiles = read.csv(inputListOfFiles, header = TRUE)
 listOfFailedFiles = c();
+listOfAlreadyExistedFiles = c();
 Num = length(listOfFiles$filename);
 
 
-for (i in 118:Num)
+for (i in 1:Num)
 {
   print(sprintf(" %d out of %d",i,Num));
   rawInputFile = as.character(listOfFiles$filename[i]);
   
   if (!file.exists(rawInputFile))
   {
-    c(listOfFailedFiles,rawInputFile)
+    listOfFailedFiles = c(listOfFailedFiles,rawInputFile)
     next;
   }
   
+
   
   filteredInputFile = paste(substr(rawInputFile,1,nchar(rawInputFile)-4),"_filtered.txt",sep="");
-  
-  
-  #smoothedInputfile = paste(substr(filteredInputFile,1,nchar(filteredInputFile)-4),"_smoothed.txt",sep="");
+  smoothedInputfile = paste(substr(filteredInputFile,1,nchar(filteredInputFile)-4),"_smoothed.txt",sep="");
+  #this is for the problem that I had for some of the smoothed file since there was a bug in the smoothSticklebackTrackFile function
+  if(file.exists(smoothedInputfile))
+  {
+    
+    listOfAlreadyExistedFiles = c(listOfAlreadyExistedFiles, smoothedInputfile);
+    next;
+    
+  }
   
   firstFrame = as.numeric(listOfFiles$startframe[i]);
   lastFrame = as.numeric(listOfFiles$stopframe[i]);
@@ -58,3 +66,6 @@ for (i in 118:Num)
 listOfFailedFileName = paste(substr(inputListOfFiles,1,nchar(inputListOfFiles)-4),"_failed.txt",sep="");
 write.csv(listOfFailedFiles, file = listOfFailedFileName)
 
+
+listOfAlreadyExistedFilesName = paste(substr(inputListOfFiles,1,nchar(inputListOfFiles)-4),"_failed.txt",sep="");
+write.csv(listOfAlreadyExistedFiles, file = listOfAlreadyExistedFilesName)
